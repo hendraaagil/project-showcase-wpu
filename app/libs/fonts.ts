9cdca9ded0e761e2c33730b4fcb00a86236448b3
hendraaagil <hendraaagil@gmail.com>
2023-11-05 14:42:50 +0700

    chore: cleanup & change font

---
+++ app/libs/fonts.ts
@@ -0,0 +1,7 @@
+import { Plus_Jakarta_Sans } from 'next/font/google'
+
+export const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['sans-serif'],
+})
