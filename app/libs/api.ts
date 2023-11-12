32dd5eb5246dab8d82cf41c9f5de153035461584
hendraaagil <hendraaagil@gmail.com>
2023-11-12 14:19:48 +0700

    fix: don't store cache

--- app/libs/api.ts
+++ app/libs/api.ts
@@ -1,6 +1,8 @@
+import { unstable_noStore as noStore } from 'next/cache'
 import { apiUrl } from '@/app/constants/url'
 
 export const fetchProjects = async () => {
+  noStore()
   const data = await fetch(apiUrl)
   const dataJson = await data.json()
 
