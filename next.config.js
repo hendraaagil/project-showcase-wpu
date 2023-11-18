a41e4353af4ee2f6f3c06e7c977f3e92f9f9c28c
hendraaagil <hendraaagil@gmail.com>
2023-11-18 18:13:32 +0700

    chore: unoptimize image

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.project-kalian.rmecha.my.id',
-        pathname: '/img/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-        pathname: '/emojis/**',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
